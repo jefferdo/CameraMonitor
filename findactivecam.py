2bf170a5e6df38f7b633681517575efd316a50c9
Jeewaka Fernando <jeewakaphotography@gmail.com>
2020-05-03 02:13:16 +0530

    update

--- findactivecam.py
+++ findactivecam.py
@@ -1,7 +1,26 @@
 import cv2
 import time
 import warnings
+import requests
+from requests.exceptions import HTTPError
+from datetime import datetime
+import json
 warnings.simplefilter("ignore")
+
+server_url = ""
+
+
+def setStatus(self, hwid, status):
+    payload = {
+        'hwid': str(hwid),
+        'timestamp': datetime.timestamp(datetime.now()),
+        'status': status
+    }
+    headers = {'content-type': 'application/json'}
+    response = requests.request(
+        "POST", server_url, data=json.dumps(payload), headers=headers)
+    return json.loads(response.text)
+
 
 def monitorCam():
     while(True):
@@ -15,7 +34,6 @@ def monitorCam():
                 if(not stat):
                     raise IOError("cant Open")
         except Exception as ex:
-
             print("\nerror found:", ex, "\n")
         time.sleep(5)
 
---
+++ test.py
@@ -0,0 +1,7 @@
+
+from datetime import datetime
+
+# current date and time
+
+timestamp = datetime.timestamp(datetime.now())
+print("timestamp =", timestamp)
